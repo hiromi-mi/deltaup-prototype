90863f40fddab8447ef054771d367fb72636f709
hiromi-mi <hiromi-mi@cat.zaq.jp>
2020-11-18 12:12:08 +0900

    patch.c 警告を消す

--- patch.c
+++ patch.c
@@ -19,12 +19,7 @@ int main(int argc, const char **argv) {
       perror("fopen");
       exit(-1);
    }
-   size_t index = 0;
-   size_t prev_index = 0;
-   unsigned char index_delta;
    char new_num;
-   size_t cnt;
-   Action action;
    size_t i = 0;
    long long len;
    long long insertlen;
@@ -46,85 +41,17 @@ int main(int argc, const char **argv) {
          break;
       }
       assert(len >= 0);
-      for (int j = 0; j < len; j++) {
+      for (long long j = 0; j < len; j++) {
          assert(fread(&new_num, 1, 1, fp) > 0);
          putchar(new_num + orig[i]);
          i++;
       }
 
-      for (unsigned long j = 0; j < insertlen; j++) {
+      for (long long j = 0; j < insertlen; j++) {
          assert(fread(&new_num, 1, 1, fp) > 0);
          putchar(new_num);
       }
    }
-   /*
-   for (cnt = 0; !feof(fp); cnt++) {
-      prev_index = index;
-
-      if (fread(&action, sizeof(Action), 1, fp) == 0)
-         break;
-      if ((Action)action == SEEK) {
-         fread(&index, sizeof(index), 1, fp);
-         fread(&i, 4, 1, fp);
-         continue;
-      }
-      assert(fread(&index_delta, sizeof(index_delta), 1, fp) > 0);
-      index = index_delta + prev_index;
-      //      unsigned int act1 = index_delta & (1 << 7);
-      //      index_delta &= ~(1 << 7);
-      // fprintf(stderr, "%ld %u %ld\n", prev_index, index_delta, index);
-      if (ferror(fp) != 0) {
-         fputs("Error!\n", stderr);
-         break;
-      }
-
-      assert(fread(&len, 4, 1, fp) > 0);
-
-      //      unsigned int act2 = len & (1 << 7);
-      //      len &= ~(1 << 7);
-      //      if (act1 == 0) {
-      //         if (act2 == 0) {
-      //            action = SEEK;
-      //         } else {
-      //            action = SUBSTITUTE;
-      //         }
-      //      } else {
-      //         if (act2 == 0) {
-      //            action = INSERT;
-      //         } else {
-      //            action = DELETE;
-      //         }
-      //      }
-      switch ((Action)action) {
-         case SUBSTITUTE:
-         case ADD:
-            for (unsigned long j = 0; j < len; j++) {
-               assert(fread(&new_num, 1, 1, fp) >= 0);
-               if ((Action)action == SUBSTITUTE) {
-                  putchar(new_num);
-               } else if ((Action)action == ADD) {
-                  putchar(new_num + orig[i]);
-               }
-               i++;
-            }
-            // fscanf(fp, "\n");
-            break;
-         case INSERT:
-            for (unsigned long j = 0; j < len; j++) {
-               assert(fread(&new_num, 1, 1, fp) >= 0);
-               putchar(new_num);
-            }
-            break;
-         case DELETE:
-            i += len;
-            break;
-         default:
-            fprintf(stderr, "Error: %d on Commond No. %ld\n", action, cnt);
-            exit(-1);
-      }
-   }
-   // fprintf(stderr, "cnt: %ld\n", cnt);
-   */
    fclose(fp);
    return 0;
 }
